67137ad1a33eb2090e4adb56e559f8140fc1f427
dgvnorth <campello03@hotmail.com>
2019-07-11 10:00:10 +0100

    Sign In  page added

--- App.js
+++ App.js
@@ -1,18 +1,28 @@
 // In App.js in a new project
 
 import React from "react";
-import { View, Text, Button } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity
+} from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
-
 
 class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text>Home Screen</Text>
         <Button
           title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
+          onPress={() => this.props.navigation.navigate("Details")}
+        />
+        <Button
+          title="SignIn"
+          onPress={() => this.props.navigation.navigate("SignIn")}
         />
       </View>
     );
@@ -29,10 +39,157 @@ class DetailsScreen extends React.Component {
   }
 }
 
+class SingInScreen extends React.Component {
+  state = {
+    name: "",
+    username: "",
+    email: "",
+    password: ""
+  };
+
+  handleNameInput = nameInput => {
+    this.setState(prevState => {
+      return {
+        name: nameInput
+      };
+    });
+  };
+
+  handleUsernameInput = usernameInput => {
+    this.setState(prevState => {
+      return {
+        username: usernameInput
+      };
+    });
+  };
+
+  handleEmailInput = emailInput => {
+    this.setState(prevState => {
+      return {
+        email: emailInput
+      };
+    });
+  };
+
+  handlePasswordInput = passwordInput => {
+    this.setState(prevState => {
+      return {
+        password: passwordInput
+      };
+    });
+  };
+
+  handleSubmit = () => {
+    console.log(this.state);
+    fetch("url", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: this.state.name,
+        username: this.state.username,
+        email: this.state.emailInput,
+        password: this.state.passwordInput
+      })
+    });
+  };
+
+  render() {
+    return (
+      <View>
+        <View>
+          <TextInput
+            style={styles.textInput}
+            placeholder="enter name"
+            maxLength={40}
+            value={this.state.name}
+            onChangeText={this.handleNameInput}
+          />
+        </View>
+        <View>
+          <TextInput
+            style={styles.textInput}
+            placeholder="enter username"
+            maxLength={40}
+            value={this.state.username}
+            onChangeText={this.handleUsernameInput}
+          />
+        </View>
+        <View>
+          <TextInput
+            style={styles.textInput}
+            placeholder="enter email address"
+            maxLength={40}
+            value={this.state.email}
+            onChangeText={this.handleEmailInput}
+          />
+        </View>
+        <View>
+          <TextInput
+            style={styles.textInput}
+            placeholder="enter password"
+            maxLength={40}
+            value={this.state.password}
+            onChangeText={this.handlePasswordInput}
+          />
+          <View style={styles.inputContainer}>
+            <TouchableOpacity
+              style={styles.saveButton}
+              onPress={this.handleSubmit}
+            >
+              <Text style={styles.saveButtonText}>Sign In</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 45,
+    backgroundColor: "#F5FCFF"
+  },
+  header: {
+    fontSize: 25,
+    textAlign: "center",
+    margin: 10,
+    fontWeight: "bold"
+  },
+  inputContainer: {
+    paddingTop: 15
+  },
+  textInput: {
+    borderColor: "#CCCCCC",
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    height: 50,
+    fontSize: 25,
+    paddingLeft: 20,
+    paddingRight: 20
+  },
+  saveButton: {
+    borderWidth: 1,
+    borderColor: "#007BFF",
+    backgroundColor: "#007BFF",
+    padding: 15,
+    margin: 5
+  },
+  saveButtonText: {
+    color: "#FFFFFF",
+    fontSize: 20,
+    textAlign: "center"
+  }
+});
+
 const AppNavigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Details: DetailsScreen
+    Details: DetailsScreen,
+    SignIn: SingInScreen
   },
   {
     initialRouteName: "Home"
