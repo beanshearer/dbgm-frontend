99d1b549b5fb827e8bf38902e439935e9ca4fb14
kenigreg <gregoryoghenah@yahoo.co.uk>
2019-07-16 12:20:43 +0100

    completed styling Splash Screen and Register Screen

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
-import { createStackNavigator, createAppContainer } from "react-navigation";
-import React from "react";
-import MapScreen from "./components/map-screen/MapScreen";
-import CaptureScreen from "./components/capture-screens/CaptureScreen";
-import UploadToChannels from "./components/capture-screens/UploadToChannels"
-import GalleryScreen from "./components/capture-screens/GalleryScreen"
-import NavigationScreen from "./components/navigation-screen/NavigationScreen"
-import AllChannels from "./components/all-channels-screen/AllChannels"
-import RegisterScreen from "./components/register-screen/RegisterScreen"
-import SignInScreen from "./components/sign-in-screen/SignInScreen"
-import SingleChannelScreen from "./components/single-channel-screen/SingleChannelScreen"
-import SingleImageScreen from "./components/single-image-screen/SingleImageScreen"
-import HomeScreen from "./components/home-screen/HomeScreen"
-import AccountManagementScreen from "./components/acount-management-screen/AccountManagementScreen"
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import React from 'react';
+import MapScreen from './components/map-screen/MapScreen';
+import CaptureScreen from './components/capture-screens/CaptureScreen';
+import UploadToChannels from './components/capture-screens/UploadToChannels';
+import GalleryScreen from './components/capture-screens/GalleryScreen';
+import NavigationScreen from './components/navigation-screen/NavigationScreen';
+import AllChannels from './components/all-channels-screen/AllChannels';
+import RegisterScreen from './components/register-screen/RegisterScreen';
+import SignInScreen from './components/sign-in-screen/SignInScreen';
+import SingleChannelScreen from './components/single-channel-screen/SingleChannelScreen';
+import SingleImageScreen from './components/single-image-screen/SingleImageScreen';
+import HomeScreen from './components/home-screen/HomeScreen';
+import AccountManagementScreen from './components/acount-management-screen/AccountManagementScreen';
+import SplashScreen from './components/splash-screen/SplashScreen';
 
 const AppNavigator = createStackNavigator(
   {
@@ -26,10 +27,11 @@ const AppNavigator = createStackNavigator(
     MapScreen,
     CaptureScreen,
     UploadToChannels,
-    GalleryScreen
+    GalleryScreen,
+    SplashScreen
   },
   {
-    initialRouteName: "NavigationScreen"
+    initialRouteName: 'NavigationScreen'
   }
 );
 
--- components/register-screen/RegisterScreen.js
+++ components/register-screen/RegisterScreen.js
@@ -1,156 +1,131 @@
-import React from "react";
+import React from 'react';
 import {
-    View,
-    Text,
-    StyleSheet,
-    TextInput,
-    TouchableOpacity,
-} from "react-native";
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView
+} from 'react-native';
 
 export default class RegisterScreen extends React.Component {
-    state = {
-        name: "",
-        username: "",
-        email: "",
-        password: ""
-    };
+  state = {
+    name: '',
+    username: '',
+    email: '',
+    password: ''
+  };
 
-    handleNameInput = nameInput => {
-        this.setState(prevState => {
-            return {
-                name: nameInput
-            };
-        });
-    };
+  handleSubmit = () => {
+    fetch('https://ea862c3d.ngrok.io/users', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: {
+          name: this.state.name,
+          username: this.state.username,
+          email: this.state.email,
+          password: this.state.password
+        }
+      })
+    });
+  };
 
-    handleUsernameInput = usernameInput => {
-        this.setState(prevState => {
-            return {
-                username: usernameInput
-            };
-        });
-    };
-
-    handleEmailInput = emailInput => {
-        this.setState(prevState => {
-            return {
-                email: emailInput
-            };
-        });
-    };
-
-    handlePasswordInput = passwordInput => {
-        this.setState(prevState => {
-            return {
-                password: passwordInput
-            };
-        });
-    };
-
-    handleSubmit = () => {
-        fetch("https://ea862c3d.ngrok.io/users", {
-            method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                username: {
-                    name: this.state.name,
-                    username: this.state.username,
-                    email: this.state.emailInput,
-                    password: this.state.passwordInput
-                }
-            })
-        });
-    };
-
-    render() {
-        return (
-            <View>
-                <View>
-                    <TextInput
-                        style={styles.textInput}
-                        placeholder="enter name"
-                        maxLength={40}
-                        value={this.state.name}
-                        onChangeText={this.handleNameInput}
-                    />
-                </View>
-                <View>
-                    <TextInput
-                        style={styles.textInput}
-                        placeholder="enter username"
-                        maxLength={40}
-                        value={this.state.username}
-                        onChangeText={this.handleUsernameInput}
-                    />
-                </View>
-                <View>
-                    <TextInput
-                        style={styles.textInput}
-                        placeholder="enter email address"
-                        maxLength={40}
-                        value={this.state.email}
-                        onChangeText={this.handleEmailInput}
-                    />
-                </View>
-                <View>
-                    <TextInput
-                        style={styles.textInput}
-                        placeholder="enter password"
-                        maxLength={40}
-                        value={this.state.password}
-                        onChangeText={this.handlePasswordInput}
-                    />
-                    <View style={styles.inputContainer}>
-                        <TouchableOpacity
-                            style={styles.saveButton}
-                            onPress={this.handleSubmit}
-                        >
-                            <Text style={styles.saveButtonText}>Sign In</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </View>
-        );
-    }
+  render() {
+    const { name, password, username, email } = this.state;
+    return (
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <Text style={styles.header}>Register</Text>
+        <TextInput
+          style={styles.textInput}
+          maxLength={40}
+          returnKeyLabel="next"
+          placeholder="Name"
+          onChangeText={name => this.setState({ name })}
+          value={name}
+          underlineColorAndroid={'transparent'}
+        />
+        <TextInput
+          style={styles.textInput}
+          returnKeyLabel="next"
+          maxLength={40}
+          placeholder="Username"
+          onChangeText={username => this.setState({ username })}
+          value={username}
+          underlineColorAndroid={'transparent'}
+        />
+        <TextInput
+          style={styles.textInput}
+          returnKeyLabel="next"
+          maxLength={40}
+          placeholder="E-mail"
+          keyboardType="email-address"
+          onChangeText={email => this.setState({ email })}
+          value={email}
+          underlineColorAndroid={'transparent'}
+        />
+        <TextInput
+          style={styles.textInput}
+          returnKeyLabel="go"
+          maxLength={40}
+          placeholder="Password"
+          onChangeText={password => this.setState({ password })}
+          value={password}
+          secureTextEntry={true}
+          underlineColorAndroid={'transparent'}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={this.handleSubmit}
+          onPress={() => this.props.navigation.navigate('AllChannels')}
+        >
+          <Text style={styles.btntext}>SIGN UP</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    );
+  }
 }
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: 45,
-        backgroundColor: "#F5FCFF"
-    },
-    header: {
-        fontSize: 25,
-        textAlign: "center",
-        margin: 10,
-        fontWeight: "bold"
-    },
-    inputContainer: {
-        paddingTop: 15
-    },
-    textInput: {
-        borderColor: "#CCCCCC",
-        borderTopWidth: 1,
-        borderBottomWidth: 1,
-        height: 50,
-        fontSize: 25,
-        paddingLeft: 20,
-        paddingRight: 20
-    },
-    saveButton: {
-        borderWidth: 1,
-        borderColor: "#007BFF",
-        backgroundColor: "#007BFF",
-        padding: 15,
-        margin: 5
-    },
-    saveButtonText: {
-        color: "#FFFFFF",
-        fontSize: 20,
-        textAlign: "center"
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#CA7E8D',
+    paddingLeft: 60,
+    paddingRight: 60
+  },
+  regform: {
+    alignSelf: 'stretch'
+  },
+  header: {
+    fontSize: 25,
+    color: 'white',
+    paddingBottom: 10,
+    marginBottom: 40,
+    borderBottomColor: 'white',
+    borderBottomWidth: 2
+  },
+  textInput: {
+    alignSelf: 'stretch',
+    height: 40,
+    padding: 10,
+    marginBottom: 10,
+    color: 'white',
+    borderBottomColor: 'white',
+    borderBottomWidth: 2
+  },
+  button: {
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#E6B655',
+    marginTop: 20,
+    marginBottom: 90
+  },
+  btntext: {
+    color: 'white',
+    fontWeight: 'bold'
+  }
 });
