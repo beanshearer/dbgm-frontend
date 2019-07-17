f07860aa9e85668be3209a62829b37df7c86457d
ladysneezes <franktmike@gmail.com>
2019-07-17 21:38:36 +0100

    all channels

--- components/all-channels-screen/AllChannels.js
+++ components/all-channels-screen/AllChannels.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
-import CheckBox from 'react-native-check-box'
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import CheckBox from "react-native-check-box";
 import * as firebase from "firebase/app";
-import "firebase/auth"
-import registerForPushNotificationsAsync from "../../registerForPushNotificationsAsync"
+import "firebase/auth";
+import registerForPushNotificationsAsync from "../../registerForPushNotificationsAsync";
 
 export default class AllChannels extends React.Component {
   state = {
@@ -12,31 +12,47 @@ export default class AllChannels extends React.Component {
     channels: {},
     channel_names: [],
     chosen: {},
-    selected_channels: [],
+    selected_channels: []
   };
 
   static navigationOptions = ({ navigation }) => {
     return {
       headerTitle: <Text>CHANNELS</Text>,
       headerLeft: (
-        <Image source={require('../../logos/logo-transparent-background.png')} style={{ width: 40, height: 40 }} />
+        <Image
+          source={require("../../logos/logo-transparent-background.png")}
+          style={{ width: 40, height: 40 }}
+        />
       ),
       headerRight: (
-        <View style={{ flex: 1, flexDirection: 'row' }}>
-          <TouchableOpacity onPress={() => { navigation.navigate('HomeScreen') }}>
-            <Image source={require('../../buttons/home.png')} style={{ width: 40, height: 40 }} />
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("HomeScreen");
+            }}
+          >
+            <Image
+              source={require("../../buttons/home.png")}
+              style={{ width: 40, height: 40 }}
+            />
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => { navigation.navigate('CaptureScreen') }}>
-            <Image source={require('../../buttons/camera.png')} style={{ width: 40, height: 40 }} />
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("CaptureScreen");
+            }}
+          >
+            <Image
+              source={require("../../buttons/camera.png")}
+              style={{ width: 40, height: 40 }}
+            />
           </TouchableOpacity>
         </View>
-      ),
+      )
     };
   };
 
-
   getChannel = () => {
-    return fetch('https://ea862c3d.ngrok.io/channels')
+    return fetch("https://ea862c3d.ngrok.io/channels")
       .then(response => response.json())
       .then(responseJson => {
         this.setState({
@@ -49,23 +65,24 @@ export default class AllChannels extends React.Component {
       });
   };
 
-  getUserByUsername = (username) => {
+  getUserByUsername = username => {
     return fetch(`https://ea862c3d.ngrok.io/users/${username}`)
       .then(response => response.json())
       .then(loggedUser => {
         this.setState({ loggedUser });
-        return loggedUser
-      }).then(loggedUser => {
-        registerForPushNotificationsAsync(loggedUser)
-        console.log(loggedUser.subscribed_channels)
-        let channels = ""
+        return loggedUser;
+      })
+      .then(loggedUser => {
+        registerForPushNotificationsAsync(loggedUser);
+        console.log(loggedUser.subscribed_channels);
+        let channels = "";
         if (loggedUser.subscribed_channels.length > 0) {
-          channels = JSON.parse(loggedUser.subscribed_channels)
+          channels = JSON.parse(loggedUser.subscribed_channels);
           channels.map(channel => {
             this.setState({
               chosen: { ...this.state.chosen, [channel]: true }
-            })
-          })
+            });
+          });
         }
       })
       .catch(error => {
@@ -73,79 +90,81 @@ export default class AllChannels extends React.Component {
       });
   };
 
-  onClick = (channel) => {
+  onClick = channel => {
     const { chosen } = this.state;
     if (!chosen[channel]) {
       return this.setState({
         chosen: { ...chosen, [channel]: true }
-      })
+      });
     } else
       this.setState({
         chosen: { ...chosen, [channel]: false }
       });
-  }
+  };
 
   render() {
     const { channel_names, loggedUser } = this.state;
 
     return (
-      <View style={{ flex: 1, alignItems: 'center', backgroundColor: "#ADDDCE" }}>
-        <Text
-          style={{ alignSelf: "stretch", margin: 5, padding: 5 }}
-        >{loggedUser.name} - Subscribed Channels</Text>
-        <View style={{ flex: 1, alignItems: 'left', alignSelf: "stretch" }}>
+      <View
+        style={{ flex: 1, alignItems: "center", backgroundColor: "#ADDDCE" }}
+      >
+        <Text style={{ alignSelf: "stretch", margin: 5, padding: 5 }}>
+          {loggedUser.name} - Subscribed Channels
+        </Text>
+        <View style={{ flex: 1, alignItems: "center", alignSelf: "stretch" }}>
           {channel_names.map(channel => {
-            return <View
-              key={channel}
-              style={{ flex: 1, alignSelf: "stretch", margin: 5, padding: 15, backgroundColor: "#E6B655", borderRadius: 5 }}
-            >
-              <CheckBox
-                onClick={() => this.onClick(channel)}
-                isChecked={this.state.chosen[channel]}
-                rightText={channel}
-              />
-            </View>
+            return (
+              <View
+                key={channel}
+                style={{
+                  flex: 1,
+                  alignSelf: "stretch",
+                  margin: 5,
+                  padding: 15,
+                  backgroundColor: "#E6B655",
+                  borderRadius: 5
+                }}
+              >
+                <CheckBox
+                  onClick={() => this.onClick(channel)}
+                  isChecked={this.state.chosen[channel]}
+                  rightText={channel}
+                />
+              </View>
+            );
           })}
         </View>
-      </View >
+      </View>
     );
   }
 
   componentDidMount() {
     this.getChannel();
-    let username = ""
-    firebase.auth().onAuthStateChanged((user) => {
+    let username = "";
+    firebase.auth().onAuthStateChanged(user => {
       if (user) {
-        username = user.displayName
-        this.setState({ username })
-        this.getUserByUsername(username)
+        username = user.displayName;
+        this.setState({ username });
+        this.getUserByUsername(username);
       } else {
-        this.setState({ username: "" })
+        this.setState({ username: "" });
       }
-    })
-    return fetch("https://exp.host/--/api/v2/push/send", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify([{
-        "to": "ExponentPushToken[DhnqAMOmU5m55epfiOGkX9]",
-        "sound": "default",
-        "body": "Hello world!"
-      }])
-    }).catch(err => { console.log(err) })
+    });
   }
 
   componentDidUpdate() {
     const { chosen, loggedUser } = this.state;
-    const keys = Object.keys(chosen)
+    const keys = Object.keys(chosen);
     const subscribedChannels = keys.filter(channel => {
-      return chosen[channel] === true
-    })
-    const { subscribed_channels, ...restOfUser } = loggedUser
-    const updatedUser = { ...restOfUser, subscribed_channels: JSON.stringify(subscribedChannels) }
-    console.log(updatedUser)
+      return chosen[channel] === true;
+    });
+    const { subscribed_channels, ...restOfUser } = loggedUser;
+    const updatedUser = {
+      ...restOfUser,
+      subscribed_channels: JSON.stringify(subscribedChannels)
+    };
+    console.log(updatedUser);
     return fetch("https://ea862c3d.ngrok.io/users", {
       method: "POST",
       headers: {
@@ -153,14 +172,16 @@ export default class AllChannels extends React.Component {
         "Content-Type": "application/json"
       },
       body: JSON.stringify(updatedUser)
-    }).catch(err => { console.log(err) })
+    }).catch(err => {
+      console.log(err);
+    });
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
     alignSelf: "stretch"
   }
 });
