abc456c618d54c1334cb6b979bb3f8f1c56f5559
ladysneezes <franktmike@gmail.com>
2019-07-17 17:44:00 +0100

    add async

--- registerForPushNotificationsAsync.js
+++ registerForPushNotificationsAsync.js
@@ -1,38 +1,43 @@
-import { Notifications } from 'expo';
-import * as Permissions from 'expo-permissions';
+import { Notifications } from "expo";
+import * as Permissions from "expo-permissions";
 
 export default async function registerForPushNotificationsAsync(loggedUser) {
-    const { status: existingStatus } = await Permissions.getAsync(
-        Permissions.NOTIFICATIONS
-    );
-    let finalStatus = existingStatus;
+  const { status: existingStatus } = await Permissions.getAsync(
+    Permissions.NOTIFICATIONS
+  );
+  let finalStatus = existingStatus;
 
-    // only ask if permissions have not already been determined, because
-    // iOS won't necessarily prompt the user a second time.
-    if (existingStatus !== 'granted') {
-        // Android remote notification permissions are granted during the app
-        // install, so this will only ask on iOS
-        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-        finalStatus = status;
-    }
+  // only ask if permissions have not already been determined, because
+  // iOS won't necessarily prompt the user a second time.
+  if (existingStatus !== "granted") {
+    // Android remote notification permissions are granted during the app
+    // install, so this will only ask on iOS
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    finalStatus = status;
+  }
 
-    // Stop here if the user did not grant permissions
-    if (finalStatus !== 'granted') {
-        return;
-    }
+  // Stop here if the user did not grant permissions
+  if (finalStatus !== "granted") {
+    return;
+  }
 
-    // Get the token that uniquely identifies this device
+  // Get the token that uniquely identifies this device
+  async function f() {
     let newToken = await Notifications.getExpoPushTokenAsync();
-    const { token, ...restOfUser } = loggedUser
-    const updatedUser = { ...restOfUser, token: newToken }
-    // POST the token to your backend server from where you can retrieve it to send push notifications.
-    return fetch("https://ea862c3d.ngrok.io/users", {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(updatedUser),
-    });
-}
+    const { token, ...restOfUser } = loggedUser;
+    const updatedUser = { ...restOfUser, token: newToken };
+    return updatedUser;
+  }
+  // POST the token to your backend server from where you can retrieve it to send push notifications.
 
+  f().then(updatedUser => {
+    fetch("https://ea862c3d.ngrok.io/users", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(updatedUser)
+    });
+  });
+}
