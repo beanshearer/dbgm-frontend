f8c6e3c0240c9dbbeecec05221884b42c24ce9e9
kenigreg <gregoryoghenah@yahoo.co.uk>
2019-07-18 10:31:32 +0100

    completed HomeScreen component

--- components/home-screen/HomeScreen.js
+++ components/home-screen/HomeScreen.js
@@ -5,53 +5,123 @@ import {
   Button,
   Image,
   TouchableOpacity,
-  StyleSheet
+  StyleSheet,
+  ScrollView
 } from 'react-native';
+import {
+  Card,
+  CardTitle,
+  CardContent,
+  CardAction,
+  CardButton,
+  CardImage
+} from 'react-native-material-cards';
 
 export default class HomeScreen extends React.Component {
+  state = { images: [], image_id: [] };
+
+  componentDidMount() {
+    this.getImages();
+  }
+
   render() {
+    const { images } = this.state;
+
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>NearBy</Text>
-      </View>
+      <ScrollView
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          position: 'relative',
+          alignContent: 'space-between',
+          flexWrap: 'wrap',
+          marginLeft: 20,
+          marginRight: 20,
+          marginTop: 10,
+          marginBottom: 10,
+          paddingBottom: 20,
+          backgroundColor: '#F4F6F4'
+        }}
+      >
+        {this.state.images &&
+          images.map(image => {
+            return (
+              <TouchableOpacity
+                onPress={() =>
+                  this.props.navigation.navigate('SingleImageScreen', {
+                    imgId: image.id
+                  })
+                }
+                key={image.id}
+              >
+                <Card>
+                  <CardImage
+                    source={{ uri: image.event_img }}
+                    title=""
+                    style={{
+                      height: 250,
+                      width: 450
+                    }}
+                  />
+                  <CardTitle
+                    title={image.username}
+                    subtitle={image.relevant_channels}
+                    style={{ fontSize: 15, fontWeight: 'bold' }}
+                  />
+                  <CardContent text={image.caption} />
+                  <CardAction separator={true} inColumn={false}>
+                    <CardButton onPress={() => {}} title="" color="blue" />
+                  </CardAction>
+                </Card>
+              </TouchableOpacity>
+            );
+          })}
+      </ScrollView>
     );
   }
+
+  getImages = () => {
+    return fetch('https://ea862c3d.ngrok.io/images')
+      .then(response => response.json())
+      .then(responseJson => {
+        let myImage = Object.values(responseJson);
+        let myImage_id = Object.keys(responseJson);
+        console.log(myImage);
+        this.setState({
+          images: myImage,
+          image_id: myImage_id
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    paddingTop: 45,
+    marginTop: 20,
     backgroundColor: '#F5FCFF'
   },
-  header: {
-    fontSize: 25,
-    textAlign: 'center',
-    margin: 10,
-    fontWeight: 'bold'
+  card: {
+    backgroundColor: '#FFF',
+    marginBottom: 10,
+    marginLeft: '2%',
+    width: 50,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    shadowRadius: 1,
+    shadowOffset: {
+      width: 3,
+      height: 3
+    }
   },
-  inputContainer: {
-    paddingTop: 15
+  cardImage: {
+    width: 500,
+    height: 500
   },
-  textInput: {
-    borderColor: '#CCCCCC',
-    borderTopWidth: 1,
-    borderBottomWidth: 1,
-    height: 50,
-    fontSize: 25,
-    paddingLeft: 20,
-    paddingRight: 20
-  },
-  saveButton: {
-    borderWidth: 1,
-    borderColor: '#007BFF',
-    backgroundColor: '#007BFF',
-    padding: 15,
-    margin: 5
-  },
-  saveButtonText: {
-    color: '#FFFFFF',
-    fontSize: 20,
-    textAlign: 'center'
+  cardText: {
+    fontSize: 15,
+    padding: 10
   }
 });
