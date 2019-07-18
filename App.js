c22d20a5f76d94f48826465e12c5ffeff4990ca1
ladysneezes <franktmike@gmail.com>
2019-07-18 11:49:16 +0100

    add map marker

--- App.js
+++ App.js
@@ -1,26 +1,25 @@
-
 import { createStackNavigator, createAppContainer } from "react-navigation";
-import { Image } from 'react-native';
+import { Image } from "react-native";
 import React from "react";
 import MapScreen from "./components/map-screen/MapScreen";
 import CaptureScreen from "./components/capture-screens/CaptureScreen";
-import UploadToChannels from "./components/capture-screens/UploadToChannels"
-import GalleryScreen from "./components/capture-screens/GalleryScreen"
-import NavigationScreen from "./components/navigation-screen/NavigationScreen"
-import AllChannels from "./components/all-channels-screen/AllChannels"
-import RegisterScreen from "./components/register-screen/RegisterScreen"
-import SignInScreen from "./components/sign-in-screen/SignInScreen"
-import SingleChannelScreen from "./components/single-channel-screen/SingleChannelScreen"
-import SingleImageScreen from "./components/single-image-screen/SingleImageScreen"
-import HomeScreen from "./components/home-screen/HomeScreen"
-import AccountManagementScreen from "./components/acount-management-screen/AccountManagementScreen"
+import UploadToChannels from "./components/capture-screens/UploadToChannels";
+import GalleryScreen from "./components/capture-screens/GalleryScreen";
+import NavigationScreen from "./components/navigation-screen/NavigationScreen";
+import AllChannels from "./components/all-channels-screen/AllChannels";
+import RegisterScreen from "./components/register-screen/RegisterScreen";
+import SignInScreen from "./components/sign-in-screen/SignInScreen";
+import SingleChannelScreen from "./components/single-channel-screen/SingleChannelScreen";
+import SingleImageScreen from "./components/single-image-screen/SingleImageScreen";
+import HomeScreen from "./components/home-screen/HomeScreen";
+import AccountManagementScreen from "./components/acount-management-screen/AccountManagementScreen";
 import config from "./config";
 import * as firebase from "firebase/app";
-import LoadingScreen from "./components/loading-screen/LoadingScreen"
-import SplashScreen from './components/splash-screen/SplashScreen';
+import LoadingScreen from "./components/loading-screen/LoadingScreen";
+import SplashScreen from "./components/splash-screen/SplashScreen";
+import Marker from "./components/map-screen/Marker";
 
 firebase.initializeApp(config);
-
 
 const AppNavigator = createStackNavigator(
   {
@@ -37,19 +36,20 @@ const AppNavigator = createStackNavigator(
     UploadToChannels,
     GalleryScreen,
     LoadingScreen,
-    SplashScreen
+    SplashScreen,
+    Marker
   },
   {
-    initialRouteName: 'LoadingScreen',
+    initialRouteName: "LoadingScreen",
     defaultNavigationOptions: {
       headerStyle: {
-        backgroundColor: '#E6B655',
+        backgroundColor: "#E6B655"
       },
-      headerTintColor: '#fff',
+      headerTintColor: "#fff",
       headerTitleStyle: {
-        fontWeight: 'bold',
-      },
-    },
+        fontWeight: "bold"
+      }
+    }
   }
 );
 
